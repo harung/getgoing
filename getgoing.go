db7d180c119a303ceab6dada5632d947ebb355af
Christian Hoefig <christian.hoefig@rise-world.com>
2023-04-13 15:25:56 +0200

    add parameter

--- getgoing.go
+++ getgoing.go
@@ -3,5 +3,5 @@ package getgoing
 import "fmt"
 
 func Helloberlin(name string) string {
-			return fmt.Sprintf("Hello %s, you should go to Berlin\n")	
+			return fmt.Sprintf("Hello %s, you should go to Berlin\n", name)	
 }
